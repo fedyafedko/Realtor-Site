7f77149e6dc90289812740573e6109c8aca0b21e
unknown <vladfedko35@gmail.com>
2023-07-21 17:13:13 +0300

    Update apartment page

--- front-end/ApartmentPage/apartment.js
+++ front-end/ApartmentPage/apartment.js
@@ -16,6 +16,8 @@ fetch(`http://localhost:5116/${idApartment}`, {
         const floorElement = document.getElementById('floor');
         const roomsElement = document.getElementById('number_rooms');
         const descriptionElement = document.getElementById('description');
+        const emailElement = document.getElementById('email');
+        const phoneElement = document.getElementById('phone');
         const sliderContainer = document.querySelector('.slider-container');
 
         const imageUrls = data.images.split(',');
@@ -50,19 +52,21 @@ fetch(`http://localhost:5116/${idApartment}`, {
         squareElement.innerHTML = `<img src="../Images/square.png" alt="" class="imageInfo"> ${data.square} m<sup>2</sup>`;
         floorElement.innerHTML = `<img src="../Images/floor.png" alt="" class="imageInfo"> ${data.floor}`;
         roomsElement.innerHTML = `<img src="../Images/rooms.png" alt="" class="imageInfo"> ${data.numberRoom}`;
-        descriptionElement.innerHTML = data.description;
+        descriptionElement.innerHTML = `${data.description}`;
         return fetch(`http://localhost:5116/Auth/id=${idUser}`, {
             method: 'GET'
         })
             .then(response => response.json())
             .then(data => {
-                let image = document.getElementById("profileImage");
-                image.setAttribute('src', `../ImagesForUserProfile/${data.images}`);
-                console.log(`../ImagesForUserProfile/${data.images}`)
-                document.getElementById('loginInfo').textContent =`Login: ${data.login}`;
-                document.getElementById('roleInfo').textContent = `Role: ${data.role}`;
-                document.getElementById('emailInfo').textContent = `Email: ${data.email}`;
-                document.getElementById('phoneInfo').textContent = `Phone: ${data.phone}`;
+                const loginInfoElement = document.getElementById('loginInfo');
+                const emailInfoElement = document.getElementById('emailInfo');
+                const profileImage = document.getElementById('profileImage');
+
+                emailElement.innerHTML = `<img src="https://cdn.icon-icons.com/icons2/788/PNG/512/email_icon-icons.com_64925.png" alt="" class="imageInfo"> ${data.email}`;
+                phoneElement.innerHTML = `<img src="../Images/phone.png" alt="" class="imageInfo"> ${data.phone}`;
+                loginInfoElement.textContent = data.login;
+                emailInfoElement.textContent = data.email;
+                profileImage.setAttribute('src', `../ImagesForUserProfile/${data.images}`);
             })
     })
     .catch(error => {
@@ -77,3 +81,29 @@ function handleSignOut() {
     // Перенаправте користувача на сторінку, зазначену у посиланні
     window.location.href = signOutButton.href;
 }
+const boxElement = document.querySelector('.box');
+const headerElement = document.querySelector('header');
+
+// Функція, яка оновлює позицію об'єкта відповідно до прокрутки сторінки
+function updateBoxPosition() {
+    const headerHeight = headerElement.offsetHeight;
+    const scrollPosition = window.scrollY;
+
+    // Обчислити доступний діапазон для переміщення об'єкта
+    const availableRange = 1000;
+
+    // Обчислити нову позицію об'єкта
+    const newPosition = Math.min(Math.max(0, scrollPosition - headerHeight), availableRange);
+
+    // Встановити нову позицію об'єкта
+    boxElement.style.transform = `translateY(${newPosition}px)`;
+}
+
+// Викликати функцію оновлення позиції при прокрутці сторінки
+window.addEventListener('scroll', updateBoxPosition);
+
+// Викликати функцію оновлення позиції при зміні розміру вікна
+window.addEventListener('resize', updateBoxPosition);
+
+// Викликати функцію вперше, щоб забезпечити початкову позицію об'єкта
+updateBoxPosition();
